e8629c3cdc2e2737f95becb9a48592600f40794e
Peiyun Lee <may1092200258@gmail.com>
2022-03-18 13:27:21 +0800

    add form

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import { useState } from 'react';
+
 
 function App() {
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    formData.append('num_terminals',2)
+    formData.append('w',1)
+    formData.append('w',1)
+    formData.append('line_id',4)
+    formData.append('linename',"松林-埔里")
+    formData.append('distance',.0)
+    formData.append('distance',.0)
+    formData.append('distance',.0)
+    formData.append('files[]',e.target.files[0].files[0])
+    formData.append('files[]',e.target.files[1].files[0])
+    const res = await axios.post('http://localhost:5000/api/v1/event/',formData,{
+      headers:{
+        'Accept':'application/json',
+        "Content-Type":"multipart/form-data"
+      }
+    })
+    console.log("send")
+    console.log(res)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form  onSubmit={(e) => handleSubmit(e)}>
+        <input name="files" type="file" multiple/>
+        <input name="files" type="file" multiple/>
+        <input type="submit" />
+      </form>
     </div>
   );
 }
