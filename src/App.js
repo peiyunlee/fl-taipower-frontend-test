6404253aef3dda4dae575404a47ca69044b739cb
Peiyun Lee <may1092200258@gmail.com>
2022-03-26 17:19:05 +0800

    update formData

--- src/App.js
+++ src/App.js
@@ -8,31 +8,31 @@ function App() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     const formData = new FormData(e.target);
-    formData.append('num_terminals',2)
-    formData.append('w',1)
-    formData.append('w',1)
-    formData.append('line_id',4)
-    formData.append('linename',"松林-埔里")
-    formData.append('distance',.0)
-    formData.append('distance',.0)
-    formData.append('distance',.0)
-    formData.append('files[]',e.target.files[0].files[0])
-    formData.append('files[]',e.target.files[1].files[0])
-    const res = await axios.post('http://localhost:5000/api/v1/event/',formData,{
-      headers:{
-        'Accept':'application/json',
-        "Content-Type":"multipart/form-data"
+    formData.append('num_terminals', 2)
+    formData.append('w', 2)
+    formData.append('w', 2)
+    formData.append('line_id', 1)
+    formData.append('line_name', "松林-埔里")
+    formData.append('distance', .0)
+    formData.append('distance', .0)
+    formData.append('distance', .0)
+    formData.append('user_id', 1)
+    formData.append('files[]', e.target.files[0].files[0])
+    formData.append('files[]', e.target.files[1].files[0])
+    const res = await axios.post('http://localhost:5000/api/v1/event/', formData, {
+      headers: {
+        'Accept': 'application/json',
+        "Content-Type": "multipart/form-data"
       }
-    })
-    console.log("send")
-    console.log(res)
+    }).catch((err) => { console.log(err.response); })
+      .then((res) => { console.log("send"); console.log(res); })
   }
 
   return (
     <div className="App">
-      <form  onSubmit={(e) => handleSubmit(e)}>
-        <input name="files" type="file" multiple/>
-        <input name="files" type="file" multiple/>
+      <form onSubmit={(e) => handleSubmit(e)}>
+        <input name="files" type="file" multiple />
+        <input name="files" type="file" multiple />
         <input type="submit" />
       </form>
     </div>
